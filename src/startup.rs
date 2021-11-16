d0e430aeba62ea0d0b050f3714ba543740b75b1c
Iulian Safta <iulian.safta@gmail.com>
2021-11-16 20:34:19 +0100

    Update database connection to pass the tests

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,21 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the connection in an Atomic Reference Counted pointer (ARC)
+    // each instance of the application, instead of getting a raw copy
+    // of a PgConnection, will get a pointer to one.
+    // Arc<T> is alwasy clonable, no metter who T is. Cloning ARC increments
+    // the number of active references and hads over a new copy of yhe memory address of the wrapped value.
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
