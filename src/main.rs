195b34c1ca0d20d888de285134c9cdb354c3a9cd
Iulian Safta <iulian.safta@gmail.com>
2021-11-16 19:02:49 +0100

    Added connection to Postgres. Pass tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use zero2prod::run;
+use std::net::TcpListener;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000").unwrap();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     
     format!("http://127.0.0.1:{}", port)
@@ -27,11 +29,17 @@ async fn health_check_works() {
 #[actix_rt::test]
 async fn subscribe_resturns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
-    let client = reqwest::Client::new();
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
     
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+    
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let client = reqwest::Client::new();
     let response = client
-        .post(format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &app_address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -39,6 +47,15 @@ async fn subscribe_resturns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+
+    let saved = sqlx::query!("SELECT email, name from subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
